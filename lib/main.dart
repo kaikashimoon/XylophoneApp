9fc9e4aa73865b1aa6b9aab16aaf6d851b7ad7d2
kai <leonc4667@gmail.com>
2022-09-05 16:38:37 -0400

    functions

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,18 @@ class XylophoneApp extends StatelessWidget {
     );
   }
 
+  void buildKey() {
+    Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(1);
+        },
+        style: TextButton.styleFrom(backgroundColor: Colors.red),
+        child: const Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,74 +31,17 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.red),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.orange),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.green),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.teal),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                    child: const Text(''),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
-                    child: const Text(''),
-                  ),
-                ),
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+            ],
           ),
         ),
       ),
